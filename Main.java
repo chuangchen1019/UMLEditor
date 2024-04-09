17c95323099865f77574f964a30e251215d87636
Evian Chuang <evian.chuang@gmail.com>
2024-04-10 02:34:11 +0800

    Instance issue fix

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import Editor.EditorFrame;
 
 public class Main {
     public static void main(String[] args) {
-        new EditorFrame();
+        EditorFrame editorFrame = EditorFrame.getInstance();
     }
 }
 
